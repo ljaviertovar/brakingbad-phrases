f9680b398a63799910ce1515c9cab28976824b77
ljaviertovar <luisjavier_tovar@outlook.com>
2021-02-02 22:29:42 -0600

    [ADD] get and show Phrase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from '@emotion/styled'
+
+import Phrase from './components/Phrase'
 
 const Container = styled.div` 
   display: flex;
@@ -17,12 +19,37 @@ const Button = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor:pointer;
+    background-size: 400px;
+  }
 `
 
 function App() {
+
+  const [phrase, setPhrase] = useState({})
+
+  const consultAPI = async () => {
+      const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+      const phrase = await api.json()
+
+      setPhrase(phrase[0])
+  }
+
+  useEffect(() => {
+    consultAPI()
+  }, [])
+
   return (
     <Container>
-      <Button>
+      <Phrase
+        phrase = {phrase}
+      />   
+      <Button
+        onClick={consultAPI}
+      >
         Get Phrase
     </Button>
     </Container>
